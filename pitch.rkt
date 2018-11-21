3e91bdbd03e3fceb8d4e9d7961456cd864e9b022
Eric Clack <eric@bn7.net>
2018-11-21 23:07:13 +0000

    Pitch change and better optional params

---
+++ pitch.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+;; racket-beats: a drum machine experiment in Racket
+;; Copyright Eric Clack, 2018
+;; This program is distributed under the terms of the GNU GPL
+
+;; Beats inspired by A Tribe Called Quest, Luck of Lucien
+;; and Billy Brooks, Fourty Days
+;; https://www.whosampled.com/sample/2185/A-Tribe-Called-Quest-Lucien-Revolucien-Luck-of-Lucien-Billy-Brooks-Fourty-Days/
+
+(require "lib/beats.rkt")
+
+(bpm 180)
+
+(define (pattern1)
+  (808-kick)
+  (808-hh-p)
+  (-1-)
+
+  (808-kick 1 2)
+  (808-hh-p)
+  (-1-)
+
+  (808-kick 1 4)
+  (808-hh-p)
+  (-1-)
+
+  (808-hh-p)
+  (-1-)
+)
+
+(define (pattern2)
+  (808-kick)
+  (808-hh-p)
+  (-1/2-)
+  (808-kick .5)
+  (-1/2-)
+
+  (808-kick)
+  (808-hh-p)
+  (-1/3-)
+  (808-kick .5)
+  (-1/3-)
+  (808-kick .5)
+  (-1/3-)
+
+  (808-kick)
+  (808-hh-p)
+  (-1-)
+
+
+  (808-hh-p)
+  (-1-)
+)
+
+
+
+(4-times
+ (pattern1)
+ (pattern2)
+)
